cb6b54070011d6dfa6c33910856910bce31d1136
germGame <peterqliu@gmail.com>
2024-01-10 20:42:05 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,89 @@
+## express-ml
+
+A library of convenience methods for MapLibre GL. 
+
+- Add layers concisely, taking layer properties (source, filter, paint, layout, minzoom, etc) in a shallow object, with no prefixes for layout and paint props.
+- Add layers and sources regardless of asynchronous load events
+- Add layers without source data at all (will default to an empty geojson source)
+
+### Usage
+
+```
+const map = new ExpressML(library)
+    .Map(options);
+```
+
+Instantiates a new map object.
+
+`library`: a MapLibre or MapboxGL library class
+`options`: map options object, equivalent to the one consumed by `maplibregl.Map`
+
+Returns an instance of `ExpressMLMap`, with all the features of `maplibregl.Map` plus the following.
+
+### `ExpressMLMap` API
+
+#### `.add{Circle|Line|Fill|Text|Icon|Background}(id[, options, after])`
+
+Add layer of the corresponding type, on map load (if not already loaded). 
+
+`id`: string. name of layer
+`options` (optional): a shallow object of all layer properties (except id). All parameters optional.
+
+Sources: one of the following. If none, uses an empty geojson source by default.
+`source`: string. reference to an existing layer, much like the vanilla implementation of `layer.source` 
+`geojson`: string or object (geojson). GeoJSON source by url string, or literal geojson object.
+`vector` + `source-layer`: string. vector tile source, plus source layer.
+
+most other params of `map.addLayer()` can be added similarly, without nesting into source/layout/paint sub-objects. 
+
+Symbol layers have an added wrinkle of offering text and/or icons. In layers with just one of these, `addText` and `addIcon` are convenience methods to add the obligatory symbol layer, while handling text- and icon- prefixes, respectively. To add symbol layers with both text and icons, use the vanilla `map.addLayer()` method.
+
+
+For example, these two operations are equivalent:
+
+```
+map.on('load', () => {
+    map.addLayer({
+        id: 'name', 
+        type: 'symbol',
+        minzoom: 12,
+        source: {
+            type: 'geojson',
+            data: 'https://www.example.com/data.geojson'
+        }, 
+        layout: {
+            'text-field': 'label', 
+            'symbol-placement': 'line'
+        }
+        paint: {
+            'text-color': 'blue'
+        }
+    })
+})
+
+```
+
+```
+// no load event necessary
+map.addText(                                                
+    'name', 
+    {
+        geojson: 'https://www.example.com/data.geojson',    // put geojson source directly in object
+        field: 'label',                                     // "{layer}-" prefixes handled automatically
+        placement: 'line',                                  // ...including "symbol-" prefixes, when adding text
+        minzoom: 12,                                        // root-level layer property
+        color: 'blue'                                       // paint properties too 
+    }
+)
+```
+
+see `example.html` for more usage examples.
+
+#### `.remove(id)`
+
+Remove a layer.
+`id`: string. ID of layer to remove
+
+#### `.setSourceData(id, data)`
+
+Shorthand for `map.getSource(id).setData(data)`. GeoJSON sources only.
---
+++ index.js
@@ -0,0 +1,254 @@
+class ExpressML {
+    
+    constructor(ml) {
+
+        this.library = ml;
+        return this;
+    }
+
+    Map(options) {
+
+        class ExpressMLMap extends this.library.Map {
+
+            constructor(o) {
+                super(o);
+            }
+            
+            // data structure of a layer object
+            #propType = {
+
+                root: ['minzoom', 'maxzoom', 'source', 'source-layer', 'filter'],
+
+                // custom source properties to be bundled into source object
+                source: ['geojson', 'vector'],
+
+                // any layer using these props make them layout props
+                layout: [
+                    'cap', 'join', 'miter', 'round-limit', 'sort', //lines
+                    // 'allow-overlap', 'anchor', 'field', 'font', 
+                    'visibility'
+                ],
+            
+                // props containing these strings are paint properties 
+                // (specifically used for symbol layers)
+                symbolPaints:[
+                    'color', 'opacity', 'halo', 'translate'
+                ],
+
+                symbolLayers:['text', 'icon'],
+
+                // props containing these strings need a `symbol-` prefix,
+                // even if they are added as text layers
+                obligateSymbolPrefixes: ['placement', 'sort-key', 'avoid-edges', 'spacing', 'z-order']
+            }
+
+            // trigger function if map is loaded, or
+            // on load event if it's not yet loaded.
+            // lets user call any MLExpress method before loading
+            #whenLoaded(fn) {
+                if (this.loaded() || this.initialLoad) {
+                    this.initialLoad = true; 
+                    return fn()
+                }
+                else return this.once('load', fn)   
+            }
+
+            #beta = {
+                queryFeatures(event, layer, cb) {
+                    this.on(event, e=>{
+                        const features = this
+                            .queryRenderedFeatures(e.point, {layers: [layer]})
+                        cb(features)
+                    })
+    
+                    return this
+                },
+    
+                queryOnClick(layers, cb) {
+                    this.on('click', e=>{
+                        const features = this
+                            .queryRenderedFeatures(e.point, {layers})
+                        cb(features)
+                    })
+    
+                    return this        
+                },
+                
+                bbox() {
+                    return this.getBounds().toArray()
+                    .flat();
+                }
+
+            }
+
+            addBackground(id, style, after) {
+                return this.#addGenericLayer('background', id, null, style, after)
+            }
+
+            addCircle(id, style, after) {
+                return this.#addGenericLayer('circle', id, style, after)
+            }
+
+            addLine(id, style, after) {
+                return this.#addGenericLayer('line', id, style, after)
+            }
+
+            addFill(id, style, after) {
+                return this.#addGenericLayer('fill', id, style, after)
+            }
+
+            addSymbol(id, layout, paint, after) {
+
+                const isVTSource = layout['source-layer'];
+                return this.#whenLoaded(()=>this.addLayer({
+                    id,
+                    type: 'symbol',
+                    source: this._formatSource(data, isVTSource),
+                    layout,
+                    paint
+                }, after))
+
+            }
+
+            addText(id, style, after) {
+
+                    const isVTSource = style['source-layer'];
+                    return this.#whenLoaded(()=>this.addLayer({
+                        id,
+                        type: 'symbol',
+                        ...this.#formatStyle(style, 'text')
+                    }, after))
+
+            }
+
+            addIcon(id, style, after) {
+
+                const isVTSource = style['source-layer'];
+                return this.#whenLoaded(()=>this.addLayer({
+                    id,
+                    type: 'symbol',
+                    ...this.#formatStyle(style, 'icon')
+                }, after))
+
+            }
+            remove(id) {
+                if (this.getLayer(id)) this.removeLayer(id)
+                else console.error(`.remove(): Map has no layer named "${id}"`)
+            }
+
+            #addGenericLayer(type, id, style, after) {
+
+                const isVTSource = style?.['source-layer'];
+
+                return this.#whenLoaded(()=>this.addLayer({
+                    id,
+                    type,
+                    ...this.#formatStyle(style, type)
+                }, after))
+            }
+
+            
+            setSourceData(source, data) {
+                return this.#whenLoaded(() => {
+
+                    const s = this.getSource(source);
+                    
+                    if (s) s.setData(this.#formatGeoJSON(data));
+                    else console.error(`Source "${source}" not found.`)
+                })
+
+            }
+
+            // handles geojson or geojson features array, or url to a remote geojson
+            #formatGeoJSON(raw) {
+
+                // url
+                if (typeof raw === 'string') return raw
+
+                else if (typeof raw === 'object') {
+
+                    // array
+                    if (raw.length) return {type: 'FeatureCollection', features: raw}
+                    // full geojson
+                    else return raw
+                }
+            }
+
+            // given a layer type and a property, determine whether it's a
+            // paint, layout, source, or root-level property
+            #resolvePropType(layerType, prop) {
+
+                const {root, source, layout, symbolPaints} = this.#propType
+
+                // identify root props
+                if (root.find(substring=>prop.includes(substring))) return 'root'
+
+                if (source.includes(prop)) return 'source'
+                const isSymbolLayer = ('textsymbolicon')
+                    .includes(layerType);    
+
+                let isPaintProperty;
+
+                // for symbol layers, check paintprop list 
+                if (isSymbolLayer) {
+                    isPaintProperty = symbolPaints
+                        .find(substring=>prop.includes(substring));
+                }
+
+                // for other layer types, check layout props
+                else {
+                    isPaintProperty = !layout
+                        .find(substring=>prop.includes(substring));
+                }    
+
+                return isPaintProperty ? 'paint' : 'layout'
+
+            }
+
+            #formatStyle(obj, layerType) {
+
+                const formattedStyle = {
+                    source: {
+                        type: 'geojson',
+                        data: {type: 'FeatureCollection', features: []}
+                    },
+                    layout: {}, 
+                    paint: {}
+                };
+
+                if (!obj) return formattedStyle
+
+                const {obligateSymbolPrefixes, symbolLayers} = this.#propType;
+                
+                // iterate through input properties, append their layer prefixes,
+                // and arrange them in the root, or source/layout/paint objects
+                Object.entries(obj)
+                    .forEach(([property,value]) => {
+
+                        const propType = this.#resolvePropType(layerType, property);
+                        if (!propType) console.error(`Unknown property "${property}"`)
+
+                        let prefixedProperty = `${layerType}-${property}`;
+                        if (symbolLayers.includes(layerType) && obligateSymbolPrefixes.includes(property)) prefixedProperty = `symbol-${property}`;
+
+                        // handle source parameters (geojson or vector)
+                        if (propType === 'source') formattedStyle.source = {
+                            type: property,
+                            data: value
+                        }
+
+                        // apply root properties
+                        else if (propType === 'root') formattedStyle[property] = value;
+
+                        // apply layout/paint properties
+                        else formattedStyle[propType][prefixedProperty] = value;
+                    })
+
+                return formattedStyle
+            }
+
+        }
+
+        return new ExpressMLMap(options)
+    } 
+}
